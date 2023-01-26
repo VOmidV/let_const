69e34ac17426babbe62426bb31ca96110529571c
Omid <omidvafa2019@gmail.com>
2023-01-26 12:23:22 -0500

    finished

---
+++ prac.js
@@ -0,0 +1,27 @@
+//var PI = 3.14;
+//PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+PI = 42;
+
+console.log(PI);
+
+
+//What is the difference between var and let?
+
+//There are two differences, one is that we can't not have redeclare with let but with var we can and the second on is scop rules that we can have with var but we can't have it with let 
+
+
+//What is the difference between var and const?
+
+//There are tree differences, first: can't redeclare with const, second: can't use scop rules, third: we can't have reassign with const
+
+
+//What is the difference between let and const?
+
+//There are one differences, we can't reassign with const but we can reasign with let
+
+
+//What is hoisting?
+
+//When we are using var you can access the variable name(undefined) value before it is used.
